ce2f3b10b86317deeea7cb9729fae345662f9377
vishalrawat111 <vishalrawat0458@gmail.com>
2025-06-16 09:58:08 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+// pages/index.js
+
+export default function Home() {
+  return (
+    <main style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
+      <h1>Welcome to Markdown Portfolio Generator</h1>
+      <p>Click on "Editor" in the URL or go to /editor to get started.</p>
+      <p>URL: <code>/editor</code></p>
+    </main>
+  );
+}
